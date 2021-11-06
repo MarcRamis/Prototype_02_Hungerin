6f57b43d82660fa4a2d1a665645abc0f969d969c
Marc Ramis <marc.ramis@enti.cat>
2021-11-06 16:31:11 +0100

    Jump implemented but it is so fcking rough

--- Assets/Scripts/Hungerin.cs
+++ Assets/Scripts/Hungerin.cs
@@ -6,27 +6,30 @@ public class Hungerin : MonoBehaviour
 {
     EssencialProperties m_EssencialProperties;
     [SerializeField] private Rigidbody m_RigidBody;
-    [SerializeField] private GameObject m_Grounded;
+    [SerializeField] private CharacterController m_CharacterController;
     
     [Space]
-    //private Ability m_CurrentAbility;
-    //[SerializeField] private string currentAbilityName;
-    private float speed = 300f;
-    private float jumpSpeed = 100f;
+    [SerializeField] private float speed = 300f;
+    [SerializeField] private float jumpSpeed = 100f;
+    [Space]
+    [SerializeField] private Transform m_Grounded;
     private bool isGrounded;
-
-    private float turnSmoothTime = 0.1f;
+    [SerializeField] private float groundRadius = 0.5f;
+    [SerializeField] private LayerMask groundMask;
+    [Space]
+    [SerializeField] private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
+    [Space]
+    [SerializeField] private float gravityScale = 40.0f;
+    [SerializeField] private float globalGravity = -9.81f;
 
     private void Awake()
     {
-        //m_CurrentAbility = new Normal(m_RigidBody);
     }
 
     // Here we control the player
     private void Update()
     {
-        //currentAbilityName = m_CurrentAbility.textName;
     }
 
     // Here we make physics
@@ -35,30 +38,39 @@ public class Hungerin : MonoBehaviour
         NormalMovement();
     }
 
-    //private void Movement()
-    //{
-    //    m_CurrentAbility.Movement(speed);
-    //}
     private void NormalMovement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
+        float jumpY = 0f;
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f,angle,0f);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
         
-        //isGrounded = RaycastHit();
+        isGrounded = Physics.CheckSphere(m_Grounded.position, groundRadius, groundMask);
+        Debug.Log(isGrounded);
 
         if (isGrounded)
         {
             if (Input.GetButton("Jump"))
             {
-                m_RigidBody.AddForce(new Vector3(0, jumpSpeed, 0));
+                m_RigidBody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
             }
         }
+        else
+        {
+            Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+            m_RigidBody.AddForce(gravity, ForceMode.Acceleration);
+        }
 
-        m_RigidBody.velocity = new Vector3(horizontal, 0f, vertical) * speed * Time.deltaTime;
+        m_RigidBody.velocity = new Vector3(horizontal, jumpY, vertical) * Time.deltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(m_Grounded.transform.position, groundRadius);
     }
 }
